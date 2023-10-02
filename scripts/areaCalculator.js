43c51ba05627e17c2176b4baf41cf1378fdb0252
Anik Das <onikd08@gmail.com>
2023-10-02 10:49:36 +0300

    Add basic error handling

--- scripts/areaCalculator.js
+++ scripts/areaCalculator.js
@@ -1,12 +1,16 @@
 function getInputValue(inputFiledId) {
   const inputField = document.getElementById(inputFiledId);
   const value = inputField.value;
-  return parseFloat(value);
+  if (value !== "") {
+    return parseFloat(value);
+  }
 }
 
 function calculateTriangleArea() {
   const base = getInputValue("triangle-base");
   const height = getInputValue("triangle-height");
-  const area = 0.5 * base * height;
-  console.log(area);
+  if (base && height) {
+    const area = 0.5 * base * height;
+    console.log(area);
+  }
 }
